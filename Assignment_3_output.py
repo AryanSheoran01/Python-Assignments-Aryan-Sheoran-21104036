4b2978ee76bd5194ed6d0a5cf9e7f912a0f74d5a
AryanSheoran01 <96770358+AryanSheoran01@users.noreply.github.com>
2022-02-14 15:58:37 +0530

    Add files via upload

---
+++ Assignment_3_output.py
@@ -0,0 +1,114 @@
+PS C:\Users\sa1> python -u "c:\Users\sa1\Desktop\python\Assignment_3.py"
+Answer:1
+
+Enter a word or line :
+Mississippi
+
+Calculating number of Occurrences of each character as the single word is entered by the user :
+ {'m': 1, 'i': 4, 's': 4, 'p': 2}
+
+Enter a word or line :
+Python is an interpreted high level general purpose programming language.
+
+Calculating Number of Occurrences of each word in line entered by the user:
+ {'python': 1, 'is': 1, 'an': 1, 'interpreted': 1, 'high': 1, 'level': 1, 'general': 1, 'purpose': 1, 'programming': 1, 'language.': 1}
+
+
+ Answer 2
+Please Enter Year: 1999
+Please Enter Month: 2
+Please Enter day: 28
+Next date in the format(dd/mm/yyyy) is: 1/3/1999
+
+ Answer : 3
+Please provide number of items to be added:
+3
+Please enter item(number):
+3
+Please enter item(number):
+9
+Please enter item(number):
+10
+
+Required list is :
+ [(3, 9), (9, 81), (10, 100)]
+
+Answer : 4
+
+Your grade point: 10
+Your grade is 'A+' and Outstanding performance
+
+ Answer : 5
+ ABCDEFGHIJK
+  ABCDEFGHI
+   ABCDEFG
+    ABCDE
+     ABC
+      A
+
+Answer : 6
+
+Enter name: Aryan
+Enter SID: 21104036
+{'Aryan ': 21104036}
+
+Want to Add more entry?
+Press 'Y' for YES / 'N' for NO.
+Y
+
+Enter name: Rahul
+Enter SID: 21115544
+{'Aryan ': 21104036, 'Rahul': 21115544}
+
+Want to Add more entry?
+Press 'Y' for YES / 'N' for NO.
+Y
+
+Enter name: Arjun
+Enter SID: 21105099
+{'Aryan ': 21104036, 'Rahul': 21115544, 'Arjun': 21105099}
+
+Want to Add more entry?
+Press 'Y' for YES / 'N' for NO.
+Y
+
+Enter name: Shruti
+Enter SID: 2888864
+{'Aryan ': 21104036, 'Rahul': 21115544, 'Arjun': 21105099, 'Shruti': 2888864}
+
+Want to Add more entry?
+Press 'Y' for YES / 'N' for NO.
+N
+
+ a) The Required dictionary is
+ {'Aryan ': 21104036, 'Rahul': 21115544, 'Arjun': 21105099, 'Shruti': 2888864}
+
+b) Dictionary sorted on the basis of names:
+ {'Arjun': 21105099, 'Aryan ': 21104036, 'Rahul': 21115544, 'Shruti': 2888864}
+
+c) Dictionary sorted on the basis of SID:
+ {'Shruti': 2888864, 'Aryan ': 21104036, 'Arjun': 21105099, 'Rahul': 21115544}
+
+d) Enter a SID to get it's name:21104036
+Name related to following sid is:
+ Aryan
+
+Answer: 7
+
+How many terms are to be printed in fibonacci series?
+10
+
+Required Fibonacci series is:
+ [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
+
+Average of following series is:
+ 8.8
+
+Answer: 8
+
+a) {1, 3, 5, 6, 8}
+b) {17, 3, 6, 8, 9, 13}
+c) {1, 2, 4, 5}
+d) {6, 7, 8, 9, 10}
+e) {10, 7}
+PS C:\Users\sa1>
